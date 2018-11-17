45d27c17b9a2a45a82df85a3920e66f58a83160b
AllanJone <AllanJone@users.noreply.github.com>
2018-11-17 22:12:59 +0530

    Training Completed

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,12 +14,49 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Iron Man" };
-            return View(movie);
+            //return View(movie);
+
+            ViewData["Movie"] = movie;
+            ViewBag.Movie = movie;
+
+            var customers = new List<Customer>
+            {
+                new Customer(){ Name = "Customer1" },
+                new Customer(){ Name = "Customer2" },
+                new Customer(){ Name = "Customer3" },
+                new Customer(){ Name = "Customer4" },
+                new Customer(){ Name = "Customer5" },
+                new Customer(){ Name = "Customer6" }
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
-        public ActionResult Index(int id)
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id is " + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex={0} and sortBy={1}",pageIndex,sortBy));
+        //}
+
+        [Route("movies/release/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id is " + id);
+            return Content(year + "/" + month);
         }
     }
 }
